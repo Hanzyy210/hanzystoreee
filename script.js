9cbcdbb47c50258ce58ef5b65cb4d33960c444ba
Hanzyy210 <hanzgokmakeagan@gmail.com>
2024-08-27 10:47:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function addToCart() {
+    alert("Item added to cart!");
+    // Tambahkan logika untuk menambah produk ke keranjang belanja
+}
+
+function removeFromCart() {
+    alert("Item removed from cart!");
+    // Tambahkan logika untuk menghapus produk dari keranjang belanja
+}
